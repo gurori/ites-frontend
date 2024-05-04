38954d6d6bdebad9df711afe89d34b12e6bfc16a
Vyacheslav <nazarovslava124@gmail.com>
2024-05-04 21:12:20 +0900

    installed react, tailwind and lucide

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { ChevronDown } from "lucide-react"
+
+function App() {
+
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eaque quod veniam, sed eveniet, et eius laudantium, quos ex dolore voluptates aspernatur dicta asperiores. Sequi aut quae enim magnam nulla.
+    </>
+  )
+}
+
+export default App
