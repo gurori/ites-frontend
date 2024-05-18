8cf9b47f58be44069563b5ab8bcb6eb1505f1243
Vyacheslav <nazarovslava124@gmail.com>
2024-05-18 19:05:30 +0900

    modified tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        'purple': {
+          DEFAULT : '#665BE3',
+          800 : '#3C1F63'
+
+        },
+        'yellow' : {
+          DEFAULT : '#CBB43B',
+          800 : '#EFDE68',
+        },
+        'gray' : {
+          200 : '#FAFAFA',
+          300 : '#DADADA',
+          400 : '#D9D9D9',
+          500 : '#747474',
+          600 : '#646464'
+        },
+        'black': {
+          DEFAULT : '#252628',
+          800 : '#1E1F21'
+        },
+        'pink' : {
+          400 : '#EC6C6C',
+          600 : '#B74C85'
+        },
+      }
+    },
   },
   plugins: [],
 }
