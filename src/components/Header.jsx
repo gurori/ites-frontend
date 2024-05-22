350aed340c7cfa70dc91db5abf8849cb694222f3
Vyacheslav <nazarovslava124@gmail.com>
2024-05-22 20:04:53 +0900

    modified nav menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,47 @@
 import { Link } from "react-router-dom";
 import { AlignJustify } from "lucide-react";
 import Nav from "./Nav";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 
 const Header = () => {
-     return (
-        <header className="flex items-center md:gap-6 lg:gap-8 relative lg:ml-4 mr-4">
-            <Link onClick={() => window.location.reload()}>
-                <img src="/logos/ites-purple-small.svg" alt="logo"  className="-mb-4"/>
-            </Link>
-            <select className="hidden md:block" name="" id="">
-                <option value="">РУС</option>
-                <option value="">ENG</option>
-                <option value="">САХ</option>
-            </select>
-            <div className="flex items-center ml-auto gap-4 md:gap-16">
-                <nav className="md:flex gap-16 hidden">
-                    <Nav />
-                </nav>
-                <button className="md:hidden">
-                    <AlignJustify />
-                </button>
-                <Link to="/sign-in"><button className="small bg-black text-white px-8">Войти</button></Link>
-            </div>
-        </header>
-     )
-}
+  return (
+    <header className="flex items-center md:gap-6 lg:gap-8 relative lg:ml-4 mr-4">
+      <button onClick={() => window.location.reload()}>
+        <img src="/logos/ites-purple-small.svg" alt="logo" className="-mb-4" />
+      </button>
+      <select className="hidden md:block" name="" id="">
+        <option value="">РУС</option>
+        <option value="">ENG</option>
+        <option value="">САХ</option>
+      </select>
+      <div className="flex items-center ml-auto gap-4 md:gap-16">
+        <nav className="md:flex gap-10 lg:gap-16 hidden">
+          <Nav />
+        </nav>
+        <NavigationMenu className="md:hidden">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>
+                <AlignJustify />
+              </NavigationMenuTrigger>
+              <NavigationMenuContent className="grid p-4 gap-2 w-[100px]">
+                <Nav />
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <Link to="/sign-in">
+          <button className="small bg-black text-white px-8">Войти</button>
+        </Link>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    container : {
-      center : true,
-      padding: '1rem',
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
     extend: {
       colors: {
@@ -32,7 +36,21 @@ module.exports = {
           600: "#B74C85",
         },
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+}
