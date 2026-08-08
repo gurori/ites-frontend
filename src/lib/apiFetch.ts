8fc4984df19d34f4afeffde59c729354d91b5aa5
gurori <nazarovslava124@gmail.com>
2026-08-08 17:02:20 +0300

    fix: add enviroment variable handling

--- src/lib/apiFetch.ts
+++ src/lib/apiFetch.ts
@@ -1,8 +1,15 @@
-export default async function apiFetch(apiPath: string, init: RequestInit = {}) {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${apiPath}`, {
-        ...init,
-        credentials: "include",
-    });
+export default function apiFetch(
+  apiPath: string,
+  init: RequestInit = {},
+) {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-    return response;
+  if (!apiUrl) {
+    throw new Error("NEXT_PUBLIC_API_URL is not defined");
+  }
+
+  return fetch(`${apiUrl}${apiPath}`, {
+    ...init,
+    credentials: "include",
+  });
 }
