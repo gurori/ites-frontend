24a8398aa0d4c77e4acf779c34f9eda7d2bc46ec
gurori <nazarovslava124@gmail.com>
2026-08-09 18:36:15 +0300

    style: format code

--- src/lib/hooks/useFormStates.ts
+++ src/lib/hooks/useFormStates.ts
@@ -1,17 +1,19 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 
 export const useFormStates = () => {
-  type Error = string | null;
-  const [formError, setAuthError] = useState<Error>(null);
+  type FormError = string | null;
+  const [formError, setAuthError] = useState<FormError>(null);
   const [formSuccess, setAuthSuccess] = useState(false);
 
-  function setFormStates(errorState: Error, successState?: boolean): void;
+  function setFormStates(errorState: FormError, successState?: boolean): void;
   function setFormStates(successState: boolean): void;
 
-  function setFormStates(value1: Error | boolean, value2?: boolean): void {
-    if (typeof value1 === "string" && "null") setAuthError(value1);
+  function setFormStates(value1: FormError | boolean, value2?: boolean): void {
+    if (value1 === null || typeof value1 === "string") {
+      setAuthError(value1);
+    }
     if (typeof value2 === "boolean") setAuthSuccess(value2);
     else if (typeof value1 === "boolean") setAuthSuccess(value1);
   }
