91cecade3da44ac73e00885211e2696de64c7867
gurori <nazarovslava124@gmail.com>
2026-08-10 16:52:42 +0300

    fix: remove api url

--- src/lib/apiFetch.ts
+++ src/lib/apiFetch.ts
@@ -2,13 +2,7 @@ export default function apiFetch(
   apiPath: string,
   init: RequestInit = {},
 ) {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
-  if (!apiUrl) {
-    throw new Error("NEXT_PUBLIC_API_URL is not defined");
-  }
-
-  return fetch(`${apiUrl}${apiPath}`, {
+  return fetch(apiPath, {
     ...init,
     credentials: "include",
   });
