1cc0cf86e3d9004f6fde75083da42d162bd72726
gurori <nazarovslava124@gmail.com>
2026-08-10 13:24:24 +0300

    fix: add tls rejection unauthorized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import 'react-image-crop/dist/ReactCrop.css'
+import "react-image-crop/dist/ReactCrop.css";
 import "./globals.css";
 import ProgressBar from "@/components/ui/ProgressBar";
 import { Toaster } from "@/components/ui/sonner";
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "ITes",
   description: "Платформа для проведения конкурсов МПИТ",
 };
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 export default function RootLayout({
   children,
 }: Readonly<{
