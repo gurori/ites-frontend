3f36ec6e669af1ed9ab941463ce0fc39bf11b2aa
gurori <nazarovslava124@gmail.com>
2026-08-12 16:32:35 +0300

    refactor(api): add bearer token and support Headers instance

--- src/lib/apiFetch.ts
+++ src/lib/apiFetch.ts
@@ -1,9 +1,21 @@
+export interface ApiFetchRequestInit extends RequestInit {
+  token?: string;
+}
+
 export default function apiFetch(
   apiPath: string,
-  init: RequestInit = {},
+  init: ApiFetchRequestInit = {},
 ) {
+  const { token, ...fetchInit } = init;
+  const headers = new Headers(fetchInit.headers);
+
+  if (token) {
+    headers.set("Authorization", `Bearer ${token}`);
+  }
+
   return fetch(apiPath, {
-    ...init,
     credentials: "include",
+    ...fetchInit,
+    headers,
   });
 }
