90778ddaa0dc8345a2f7e4f56e6a1f930e334638
gurori <nazarovslava124@gmail.com>
2026-08-13 08:12:08 +0300

    refactor: rewrite useFormHandler logic

--- src/lib/hooks/useFormHandler.ts
+++ src/lib/hooks/useFormHandler.ts
@@ -3,7 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useFormStates } from "./useFormStates";
 import { z } from "zod";
-import { useForm, UseFormProps } from "react-hook-form";
+import { type FieldValues, type UseFormProps, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { type IServerErrorMessage } from "../types/IServerErrorMessage";
 import { type HttpMethod } from "../types/HttpMethod";
@@ -13,21 +13,21 @@ import { useCallback, useEffect } from "react";
 export type UserRedirect = {
   type: "push" | "replace";
   href: string;
-  prefetch?: Parameters<ReturnType<typeof useRouter>["prefetch"]>;
+  prefetchOptions?: Parameters<ReturnType<typeof useRouter>["prefetch"]>[1];
 };
 
-export type UseFormHandlerProps<TSchema extends z.ZodTypeAny> = {
-  schema: TSchema;
-  apiPath: string;
+export type UseFormHandlerProps<TFormData extends FieldValues> = {
+  schema: z.ZodTypeAny;
+  apiPath?: string;
   token?: string;
   userRedirect?: UserRedirect;
   userInputError?: string;
   method?: Extract<HttpMethod, "POST" | "PUT">;
-  defaultValues?: UseFormProps<z.infer<TSchema>>["defaultValues"];
+  defaultValues?: UseFormProps<TFormData>["defaultValues"];
   fileName?: string;
 };
 
-export const useFormHandler = <TSchema extends z.ZodTypeAny>({
+export const useFormHandler = <TFormData extends FieldValues>({
   schema,
   apiPath,
   token,
@@ -36,22 +36,17 @@ export const useFormHandler = <TSchema extends z.ZodTypeAny>({
   fileName,
   userInputError = "Ошибка. Пожалуйста, повторите позже.",
   method = "POST",
-}: UseFormHandlerProps<TSchema>) => {
+}: UseFormHandlerProps<TFormData>) => {
   const { formError, formSuccess, setError, setSuccess } = useFormStates();
   const router = useRouter();
 
-  const prefetchUrl = userRedirect?.prefetch?.[0];
-  const prefetchOptions = userRedirect?.prefetch?.[1];
-
   useEffect(() => {
-    if (prefetchUrl) {
-      router.prefetch(prefetchUrl, prefetchOptions);
+    if (userRedirect) {
+      router.prefetch(userRedirect.href, userRedirect.prefetchOptions);
     }
-  }, [prefetchUrl, prefetchOptions, router]);
+  }, [userRedirect, router]);
 
-  type FormData = z.infer<TSchema>;
-
-  const form = useForm<FormData>({
+  const form = useForm<TFormData>({
     resolver: zodResolver(schema),
     defaultValues,
   });
@@ -74,9 +69,11 @@ export const useFormHandler = <TSchema extends z.ZodTypeAny>({
           return;
         }
 
-        if (
-          response.headers.get("content-type")?.includes("application/json")
-        ) {
+        const isJson = response.headers
+          .get("content-type")
+          ?.includes("application/json");
+
+        if (isJson) {
           const error: IServerErrorMessage = await response.json();
           setError(error.detail || userInputError);
         } else {
@@ -84,30 +81,43 @@ export const useFormHandler = <TSchema extends z.ZodTypeAny>({
         }
       } catch (error) {
         console.error("Submission error:", error);
-        setError("Ошибка. Пожалуйста, повторите позже.");
+        setError(userInputError);
       }
     },
     [router, userRedirect, userInputError, setError, setSuccess],
   );
 
   const onSubmit = useCallback(
-    async (data: FormData) => {
+    async (data: TFormData) => {
+      if (!apiPath) {
+        console.warn("No apiPath provided to useFormHandler");
+        return;
+      }
+
       await handleFetch(async () => {
         const isFile = Boolean(fileName);
         const formData = new FormData();
         const headers = new Headers();
+        let body: BodyInit;
 
         if (isFile) {
-          formData.append("file", (data as { file: File[] }).file[0], fileName);
+          const files = data.file as File[] | undefined;
+
+          if (files && files.length > 0) {
+            formData.append("file", files[0], fileName);
+          }
+
+          body = formData;
         } else {
           headers.set("Content-Type", "application/json");
+          body = JSON.stringify(data);
         }
 
         const response = await apiFetch(apiPath, {
-          method: method,
+          method,
           headers,
           token,
-          body: isFile ? formData : JSON.stringify(data),
+          body,
         });
 
         return response;
