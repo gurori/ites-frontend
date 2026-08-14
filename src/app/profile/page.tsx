6d53afe5c72ab24b8dc5695e8de3836d2191b7c4
gurori <nazarovslava124@gmail.com>
2026-08-14 14:21:28 +0300

    refactor: remove unused profile page

--- src/app/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import { getRole, getToken } from "@/lib/services/user";
-
-export default async function ProfilePageProvider() {
-  const token = await getToken();
-  const role = await getRole(token!);
-
-  redirect(`/profile/${role}`);
-}
