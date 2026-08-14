8be8ac646f82ed16c0b6dbddd5d60fe5fa2a7f15
gurori <nazarovslava124@gmail.com>
2026-08-14 11:44:02 +0300

    refactor: clean up fetch logic

--- src/app/main/orders/page.tsx
+++ src/app/main/orders/page.tsx
@@ -6,19 +6,20 @@ import Order from "./Order";
 export const revalidate = 10;
 
 export default async function OrdersPage() {
-  async function getOrders() {
-    const orders: IOrder[] = await apiFetch("/api/orders").then(
-      async (res) => await res.json()
-    );
-    return orders;
+  const response = await apiFetch("/api/orders");
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch orders: ${response.status}`);
   }
-  const orders = await getOrders();
+
+  const orders: IOrder[] = await response.json();
+
   return (
     <>
       <MainTabsButtons active="Заказы" />
       <div className="pt-8 md:pt-16 grid gap-16">
-        {orders.map((o) => (
-          <Order order={o} key={o.id} />
+        {orders.map((order) => (
+          <Order order={order} key={order.id} />
         ))}
       </div>
     </>
