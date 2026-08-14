f9e8413d6ab0ccf60e5b14a96e0e00bd8477ffcf
gurori <nazarovslava124@gmail.com>
2026-08-14 11:09:40 +0300

    refactor: extract Loguot layout to SSR page

--- src/app/logout/Logout.tsx
+++ src/app/logout/Logout.tsx
@@ -2,30 +2,24 @@
 
 import { useRouter } from "next/navigation";
 import { deleteCookie } from "../actions";
-import s from "./Logout.module.css";
 
 export default function Logout() {
-  const { push, back } = useRouter();
+  const { replace, back } = useRouter();
   return (
-    <div className="h-screen bg-black center">
-      <div className={s.logout}>
-        <p className="text-white">Вы уверены, что хоите выйти?</p>
-        <div className="flex gap-4 justify-self-end">
-          <button className="yellow-border px-4" onClick={back}>
-            Отмена
-          </button>
-          <button
-            className="small bg-red-400 text-black-800 px-4"
-            onClick={async () => {
-              await deleteCookie("auth");
-              await deleteCookie("role");
-              push("/");
-            }}
-          >
-            Подвердить
-          </button>
-        </div>
-      </div>
-    </div>
+    <>
+      <button className="yellow-border px-4" onClick={back}>
+        Отмена
+      </button>
+      <button
+        className="small bg-red-400 text-black-800 px-4"
+        onClick={async () => {
+          await deleteCookie("auth");
+          await deleteCookie("role");
+          replace("/");
+        }}
+      >
+        Подвердить
+      </button>
+    </>
   );
 }
--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,10 +1,19 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import Logout from "./Logout";
+import s from "./Logout.module.css";
 
 export default async function LogoutPage() {
-  if(!cookies().has("auth"))
-    redirect("/")
+  if (!cookies().has("auth")) redirect("/");
 
-  return <Logout />;
+  return (
+    <div className="h-screen bg-black center">
+      <div className={s.logout}>
+        <p className="text-white">Вы уверены, что хоите выйти?</p>
+        <div className="flex gap-4 justify-self-end">
+          <Logout />
+        </div>
+      </div>
+    </div>
+  );
 }
