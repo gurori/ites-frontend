e8fda6355c93f0267d717d68a93ea22f4304d83f
gurori <nazarovslava124@gmail.com>
2026-08-14 16:08:16 +0300

    refactor: update team info page for safe error handling

--- src/app/team/[id]/page.tsx
+++ src/app/team/[id]/page.tsx
@@ -2,19 +2,29 @@ import apiFetch from "@/lib/apiFetch";
 import type { ITeam } from "@/lib/types/ITeam";
 import { notFound } from "next/navigation";
 import TeamInfo from "./TeamInfo";
-import { getToken } from "@/lib/services/user";
+import { getRole, getToken } from "@/lib/services/user";
 
 export default async function TeamInfoPage({
   params,
 }: {
   params: { id: string };
 }) {
-    const token = await getToken("auth", false);
-  const team: ITeam = await apiFetch(`/api/teams/${params.id}`).then(
-    async (r) => {
-      if (r.status === 404) return notFound();
-      return await r.json();
-    }
-  );
-  return <TeamInfo team={team} token={token!.value} />;
+  const { id } = params;
+  const token = getToken("auth", null);
+  const role = getRole(null);
+
+  const response = await apiFetch(`/api/teams/${id}`);
+
+  if (response.status === 404) {
+    return notFound();
+  }
+
+  if (!response.ok) {
+    console.error("Failed to fetch team:", response.status);
+    throw new Error(`Failed to fetch team with status ${response.status}`);
+  }
+
+  const team: ITeam = await response.json();
+
+  return <TeamInfo team={team} token={token} role={role} />;
 }
