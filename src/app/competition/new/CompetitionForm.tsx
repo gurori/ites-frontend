2580f824abde8294ead4d19b422a2dde38d2451e
gurori <nazarovslava124@gmail.com>
2026-08-14 11:03:49 +0300

    refactor: add contentInHtml zod schema

--- src/app/competition/new/CompetitionForm.tsx
+++ src/app/competition/new/CompetitionForm.tsx
@@ -6,28 +6,34 @@ import ErrorMessage from "@/components/ui/ErrorMessage";
 import FormError from "@/components/ui/FormError";
 import { useFormHandler } from "@/lib/hooks/useFormHandler";
 import { cn } from "@/lib/utils";
-import { xlTextSchema } from "@/lib/zod-schemas";
+import { contentInHtmlSchema } from "@/lib/zod-schemas";
 import { useRouter } from "next/navigation";
 import { useController } from "react-hook-form";
 import { z } from "zod";
 import s from "@/components/info-card/InfoCard.module.css";
 
+const competitionSchema = z.object({
+  contentInHtml: contentInHtmlSchema,
+});
+
 export default function CompetitionForm({
   token,
 }: Readonly<{ token: string }>) {
   const { back } = useRouter();
-  const competitionSchema = z.object({
-    contentInHtml: xlTextSchema,
+  const {
+    control,
+    formError,
+    formState: { errors },
+    handleSubmit,
+    onSubmit,
+  } = useFormHandler({
+    schema: competitionSchema,
+    apiPath: "/api/competitions/create",
+    token,
+    userRedirect: { href: "/profile/organizer", type: "replace" },
   });
-  const { control, errors, formError, handleSubmit, onSubmit } = useFormHandler(
-    {
-      schema: competitionSchema,
-      apiPath: "/api/Competitions/create",
-      token: token,
-      pushPath: "/profile/organizer",
-    }
-  );
   const { field } = useController({ control, name: "contentInHtml" });
+
   return (
     <div className={cn(s.card, "p-4 md:p-10")}>
       <div className="flex gap-6 items-center mb-8">
