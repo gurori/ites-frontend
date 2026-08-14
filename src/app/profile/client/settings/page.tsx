4248133f6f5cf71874dd6ac34674a1aa02c188d9
gurori <nazarovslava124@gmail.com>
2026-08-14 12:45:54 +0300

    refactor:  check user role

--- src/app/profile/client/settings/page.tsx
+++ src/app/profile/client/settings/page.tsx
@@ -1,8 +1,14 @@
+import { redirect } from "next/navigation";
 import ProfileSettings from "./ProfileSettings";
-import { getToken } from "@/lib/services/user";
+import { getRole, getToken } from "@/lib/services/user";
 
 export default function SettingsPage() {
   const token = getToken()!
+  const role = getRole();
+
+  if (role !== "client") {
+    redirect(`/profile/${role}`)
+  }
   
   return <ProfileSettings token={token} />;
 }
