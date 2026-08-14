c7622132b85177e5135a381d6f13c022eb1ed099
gurori <nazarovslava124@gmail.com>
2026-08-14 09:22:01 +0300

    refactor: extract LoginForm component layout to page SSR

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
 import LoginForm from "./LoginForm";
+import styles from "./Login.module.css";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <div className="h-screen center bg-black px-4">
+      <div className={styles.whiteBox}>
+        <Image
+          src="/icons/user.png"
+          className="absolute top-0 -translate-y-1/2"
+          alt="user icon"
+          width={160}
+          height={160}
+        />
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
