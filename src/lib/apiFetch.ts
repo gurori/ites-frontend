5cb7636918178b02e367f5e6f8dad3f85cfc84f7
gurori <nazarovslava124@gmail.com>
2026-08-15 09:10:28 +0300

    fix: update server url

--- src/lib/apiFetch.ts
+++ src/lib/apiFetch.ts
@@ -2,10 +2,23 @@ export interface ApiFetchRequestInit extends RequestInit {
   token?: string;
 }
 
+const BACKEND_URL = process.env.NEXT_PUBLIC_API_PROTOCOL
+  ? `${process.env.NEXT_PUBLIC_API_PROTOCOL}://${process.env.NEXT_PUBLIC_API_HOST}${process.env.NEXT_PUBLIC_API_PORT ? `:${process.env.NEXT_PUBLIC_API_PORT}` : ""}`
+  : "https://gurori.ru";
+
+const getBaseUrl = (endpoint: string) => {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+
+  return BACKEND_URL;
+};
+
 export default function apiFetch(
-  apiPath: string,
+  endpoint: string,
   init: ApiFetchRequestInit = {},
 ) {
+  const url = `${getBaseUrl(endpoint)}${endpoint}`;
   const { token, ...fetchInit } = init;
   const headers = new Headers(fetchInit.headers);
 
@@ -13,7 +26,7 @@ export default function apiFetch(
     headers.set("Authorization", `Bearer ${token}`);
   }
 
-  return fetch(apiPath, {
+  return fetch(url, {
     credentials: "include",
     ...fetchInit,
     headers,
