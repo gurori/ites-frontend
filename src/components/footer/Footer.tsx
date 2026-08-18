6f719c8c497676ec197e6116ed0b9aa6329c64e2
gurori <nazarovslava124@gmail.com>
2026-08-18 08:55:31 +0300

    refactor: remove unused code

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,30 +1,9 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-import { CSSTransition } from "react-transition-group";
-import styles from "./Footer.module.css";
 import Image from "next/image";
-import { MailIcon, Phone } from "lucide-react";
+import { MailIcon } from "lucide-react";
 
 const Footer = () => {
-  // const phoneNumber = "+79841162646";
-  // const [isMessageVisible, setIsMessageVisible] = useState(false);
-  // const nodeRef = useRef(null);
-  // const handleCopyText = (text: string): void => {
-  //   navigator.clipboard.writeText(text);
-  //   setIsMessageVisible(true);
-  // };
-
-  // useEffect(() => {
-  //   let timeoutId: NodeJS.Timeout;
-  //   if (isMessageVisible) {
-  //     timeoutId = setTimeout(() => {
-  //       setIsMessageVisible(false);
-  //     }, 400);
-  //   }
-  //   return () => clearTimeout(timeoutId);
-  // }, [isMessageVisible]);
-
   return (
     <footer className="relative">
       <div className="bg-purple py-24">
@@ -33,61 +12,8 @@ const Footer = () => {
             <h3 className="pb-4">Остались еще вопросы?</h3>
             <p className="pt-8">Свяжитесь с нами!</p>
           </div>
-          {/* <div className="flex gap-7 items-center pt-16 pb-4">
-            <button
-              onClick={() => {
-                handleCopyText("@Gurori124");
-              }}
-            >
-              <Image
-                src="/logos/telegram.png"
-                width={49}
-                height={49}
-                alt="telegram logo"
-              />
-            </button>
-            <button
-              onClick={() => {
-                handleCopyText("No.");
-              }}
-            >
-              <Image
-                src="/logos/facebook.png"
-                width={49}
-                height={49}
-                alt="facebook logo"
-              />
-            </button>
-            <button
-              onClick={() => {
-                handleCopyText(phoneNumber);
-              }}
-            >
-              <Image
-                src="/logos/whatsapp.png"
-                width={49}
-                height={49}
-                alt="whatsapp logo"
-              />
-            </button>
-            <CSSTransition
-              in={isMessageVisible}
-              timeout={400}
-              nodeRef={nodeRef}
-              classNames={{
-                enter: styles.fadeEnter,
-                enterActive: styles.fadeEnterActive,
-                exit: styles.fadeExit,
-                exitActive: styles.fadeExitActive,
-              }}
-              unmountOnExit
-            >
-              {() => <div ref={nodeRef}>Скопировано!</div>}
-            </CSSTransition>
-          </div> */}
+          
           <div className="flex gap-2 items-center text-black pt-4">
-            {/* <Phone size={18} />
-            {phoneNumber} */}
             <MailIcon size={20} />
             itelectronicschool@gmail.com
           </div>
