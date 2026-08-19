f349eccd11901b1bd3addf676424711f05fc7260
gurori <nazarovslava124@gmail.com>
2026-08-19 12:34:47 +0300

    fix: add forwardRef and props spreading to GhostButton

--- src/components/ui/buttons/GhostButton.tsx
+++ src/components/ui/buttons/GhostButton.tsx
@@ -1,15 +1,23 @@
 import { cn } from "@/lib/utils";
-import type { ButtonHTMLAttributes, ReactNode } from "react";
+import React, { type ButtonHTMLAttributes, type ReactNode } from "react";
 import s from "./Button.module.css";
 
-export default function GhostButton({
-  children,
-  className,
-}: Readonly<
-  {
-    children: ReactNode;
-    className?: string;
-  } & ButtonHTMLAttributes<HTMLButtonElement>
->) {
-  return <button className={cn(s.ghost, className)}>{children}</button>;
-}
+const GhostButton = React.forwardRef<
+  HTMLButtonElement,
+  Readonly<
+    {
+      children: ReactNode;
+      className?: string;
+    } & ButtonHTMLAttributes<HTMLButtonElement>
+  >
+>(({ children, className, ...props }, ref) => {
+  return (
+    <button ref={ref} className={cn(s.ghost, className)} {...props}>
+      {children}
+    </button>
+  );
+});
+
+GhostButton.displayName = "GhostButton";
+
+export default GhostButton;
